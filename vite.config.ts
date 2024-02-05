08198d1968e2673b3796ca16f2dbf2e1c1368630
jwu8475 <jwu8475@gmail.com>
2024-02-04 19:11:03 -0600

    connected and setup backend co-author: jeffrey-mai jeffreymai3000@gmail.com

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/user': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/job': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+    port: 5173,
+  },
 })
